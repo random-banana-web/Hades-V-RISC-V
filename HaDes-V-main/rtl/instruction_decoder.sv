39baa8abeff841fa60f0c9f2f0511f8374332614
arnab <12345arnabghosh@gmail.com>
2026-07-16 13:51:40 +0530

    added U and J support for instruction_decoder

--- HaDes-V-main/rtl/instruction_decoder.sv
+++ HaDes-V-main/rtl/instruction_decoder.sv
@@ -9,7 +9,7 @@
 
 module instruction_decoder (
     input  logic [31:0] instruction_in,
-    output logic [6:0]  opcode,
+    output logic [6:0]  op_code,
     output logic [2:0]  funct3,
     output logic [6:0]  funct7,
     output logic [4:0]  rs1,
@@ -17,25 +17,26 @@ module instruction_decoder (
     output logic [4:0]  rd,
     output logic [31:0] imm
 );
-    assign opcode=instruction_in[6:0];
+    assign op_code=instruction_in[6:0];
     assign funct3=instruction_in[14:12];
     assign rs1=instruction_in[19:15];
+    imm=32'b0;
     always_comb begin
      // R type
-    if (opcode==7'b0110011) begin
+    if (op_code==7'b0110011) begin
          funct7=instruction_in[31:25];
          rd=instruction_in[11:7];
          rs2=instruction_in[24:20];
     end
     
     // I type
-    else if (opcode == 7'b0010011 || opcode == 7'b0000011 || opcode == 7'b1100111) begin
+    else if (op_code == 7'b0010011 || op_code == 7'b0000011 || op_code == 7'b1100111) begin
          imm[11:0]=instruction_in[31:20];
          imm[31:12]={20{imm[11]}};
          rd=instruction_in[11:7];
     end
     // S type
-    else if (opcode==7'b0100011) begin
+    else if (op_code==7'b0100011) begin
          imm[4:0]=instruction_in[11:7];
          imm[11:5]=instruction_in[31:25];
          imm[31:12]={20{imm[11]}};
@@ -43,7 +44,7 @@ module instruction_decoder (
     end
     
     // B type
-    else if (opcode==7'b1100011) begin
+    else if (op_code==7'b1100011) begin
          imm[11]=instruction_in[7];
          imm[4:1]=instruction_in[11:8];
          imm[10:5]=instruction_in[30:25];
@@ -51,5 +52,21 @@ module instruction_decoder (
          imm[31:13]={19{imm[12]}};
          rs2=instruction_in[24:20];
     end
+    //U type
+    else if (op_code==7'b0010111|| op_code==7'b0110111) begin
+     //auipc
+          rd=instruction_in[11:7];
+          imm={instruction_in[31:12],12'b0};  
     end
+    //J type
+    else if (op_code==7'b1101111) begin
+     //jal
+          rd=instruction_in[11:7];
+          imm[19:12]=instruction_in[19:12];
+          imm[11]=instruction_in[20];
+          imm[10:1]=instruction_in[30:21];
+          imm[20]=instruction_in[31];
+          imm[31:21]={11{imm[20]}};     
+    end
+end
 endmodule
